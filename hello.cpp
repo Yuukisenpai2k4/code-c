1bb4e0b73fa688f0387412cb94658de643170eb8
Your Name <you@example.com>
2019-08-18 21:28:51 +0700

    init

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+/*
+Hello world program
+written by haunv
+*/
+int main(){
+	//	Write some code inside main function
+	printf("Hello World!\n");
+	system("PAUSE");
+	return 0;
+}
