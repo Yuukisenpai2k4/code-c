4884477b543002910ace98351481deba4f5197e6
Your Name <you@example.com>
2019-08-20 09:39:15 +0700

    chuong trinh thu ba

--- hello C.c
+++
@@ -1,7 +0,0 @@
-// this is my fist c program
-#include<stdio.h>
-int main(){
-	printf("welcom to C progamming language");
-	return 0;
-	
-}
--- helloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	//write some code inside main funcion
-	printf("Hello worrld!\n");
-	getchar();
-	return 0;
-	
-}
