806dff2e0cd4065403979ce0f697ba06028ea6e3
Your Name <you@example.com>
2019-08-20 22:28:34 +0700

    Basic

---
+++ Basic/1. data types and variables/bai 4.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+	double a = 6.903267;
+	printf ("%c", a);
+	return 0;
+	
+}
---
+++ Basic/2. basic input  & output/bai 1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	char a;
+	printf("Nhap vao 1 ky tu: ");
+	
+    //	Nhap gia tri a tu ban phim
+	scanf("%c", &a);
+	
+	// In a ra man hinh
+	printf("Ky tu ban vua nhap la: %c", a);
+		
+	return 0;
+}	
+
