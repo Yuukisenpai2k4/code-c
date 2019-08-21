b3aecfe609bd93172565dfa50b284ce8e8cd2682
Your Name <you@example.com>
2019-08-21 22:15:11 +0700

    condition if,else

---
+++ Basic/4.condition if,else/Bai 1..cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	int b;
+	scanf("%d%d", &a, &b);
+	if (a<b) printf("%d<%d", a, b);
+	else if("a = b") printf("%d = %d", a, b);
+	if (a>b) printf("%d>%d", a, b);
+	return 0;
+	
+	
+}
+	 
